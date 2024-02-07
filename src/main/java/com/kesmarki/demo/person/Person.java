d3bcf0402d71863ecdf76056cbfef571fb03ba11
László Lukács <glonaabot@gmail.com>
2024-02-08 00:55:35 +0100

    Creating person class.

---
+++ src/main/java/com/kesmarki/demo/person/Person.java
@@ -0,0 +1,15 @@
+package com.kesmarki.demo.person;
+
+import com.kesmarki.demo.address.Address;
+import com.kesmarki.demo.contact.Contact;
+
+import java.util.List;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private List<Address> addresses;
+    private List<Contact> contacts;
+
+}
